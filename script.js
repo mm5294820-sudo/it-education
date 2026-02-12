b2335b727d27fd3091f7d3f97903427d391cc37f
Mohammed muzammil <mm5294820@gmail.com>
2026-02-12 12:58:31 +0500

    Add course item toggle functionality to script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let humicon=document.getElementsByClassName("fa-bars")[0]
 let menus=document.getElementsByClassName("menu")[0]
 let close=document.getElementsByClassName("fa-xmark")[0]
+let course=document.getElementsByClassName("courses")[0]
+let iconangledown=document.getElementsByClassName("fa-angle-down")[0]
+let courseItem=document.getElementsByClassName("course-item")[0]
 
 humicon.addEventListener("click",function(){
     menus.classList.add("menu-active")
@@ -12,4 +15,10 @@ close.addEventListener("click",function(){
     menus.classList.remove("menu-active")
     humicon.classList.remove("fa-bars-remove")
     document.body.classList.remove("blur-active")
+})
+
+course.addEventListener("click",function(){
+    courseItem.classList.toggle("course-items-active")
+    iconangledown.classList.toggle("fa-angle-down")
+    iconangledown.classList.toggle("fa-angle-up")
 })
