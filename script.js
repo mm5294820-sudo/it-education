3c3c10c1f6d448d135c913b7b33053e318c88740
Mohammed muzammil <mm5294820@gmail.com>
2026-02-17 13:25:30 +0500

    Implement CNIC input formatting
    Added input formatting for CNIC number.

--- script.js
+++ script.js
@@ -22,3 +22,28 @@ course.addEventListener("click",function(){
     iconangledown.classList.toggle("fa-angle-down")
     iconangledown.classList.toggle("fa-angle-up")
 })
+
+const cnicInput = document.getElementById('cnic-no');
+
+if (cnicInput) {
+    cnicInput.addEventListener('input', function (e) {
+        let input = e.target;
+
+        let value = input.value.replace(/\D/g, '').substring(0, 13);
+
+        let formattedValue = '';
+        if (value.length > 0) {
+
+            formattedValue = value.substring(0, 5);
+        }
+        if (value.length > 5) {
+
+            formattedValue += '-' + value.substring(5, 12);
+        }
+        if (value.length > 12) {
+            formattedValue += '-' + value.substring(12, 13);
+        }
+
+        input.value = formattedValue;
+    });
+}
